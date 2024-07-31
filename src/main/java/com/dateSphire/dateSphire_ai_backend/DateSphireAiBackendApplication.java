fe837c8276b06885910c4df59c260c66ee7d4fce
Aya Abdeen <mohamedabdeen898@gmail.com>
2024-07-30 19:04:11 -0500

    init DateSphire AI App

---
+++ src/main/java/com/dateSphire/dateSphire_ai_backend/DateSphireAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dateSphire.dateSphire_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateSphireAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateSphireAiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dateSphire/dateSphire_ai_backend/DateSphireAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dateSphire.dateSphire_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateSphireAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
